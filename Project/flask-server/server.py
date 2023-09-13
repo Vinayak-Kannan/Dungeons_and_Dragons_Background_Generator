9f6b8f8f737e71dcfa97e93bc25f3632d12b04cb
vinayakkannan <vk2364@columbia.edu>
2023-09-13 17:34:25 -0400

    Actually compiling code

--- Project/flask-server/server.py
+++ Project/flask-server/server.py
@@ -1,26 +1,30 @@
+import openai
 from dotenv import dotenv_values
 from flask import Flask, request
 from flask_cors import cross_origin
 
 app = Flask(__name__)
 config = dotenv_values(".env")
-SECRET_KEY = config.get('SECRET_KEY')
+openai.api_key = config.get('SECRET_KEY')
 
 
 # API Route
 @app.route("/ping")
 @cross_origin(origin="*")
 def ping():
-    return f'API_KEY = { SECRET_KEY }'
+    return f'API_KEY = { "SECRET_KEY" }'
 
 
 @app.route("/upload")
 @cross_origin(origin="*")
 def upload():
     message = request.get_data().decode("utf-8")
-
-    # do something with the message
-    return "Message received"
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=message,
+        temperature=0.7,
+    )
+    return response.choices[0].message.content
 
 
 if __name__ == "__main__":
